24d179b74e2be1a7ddaf876073106d482faa5ea6
Noel Somoza <143645214+noelsomoza@users.noreply.github.com>
2023-08-30 12:44:37 -0700

    Create Combine Data.sql

---
+++ Combine Data.sql
@@ -0,0 +1,31 @@
+--combining 13 months of data from July 2022 to July 2023
+
+CREATE TABLE radiant-ion-397303.cyclistic_dataset.combined_data AS (
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202207_tripdata
+  UNION ALL 
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202208_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202209_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202210_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202211_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202212_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202301_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202302_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202303_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202304_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202305_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202306_tripdata
+  UNION ALL
+  SELECT * FROM radiant-ion-397303.cyclistic_dataset.202307_tripdata
+
+)
+;
